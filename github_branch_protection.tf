445822fee7d8b27e198e65dc6ef0f0404a0b1b16
Sebastian Toepfer <61313468+sebastian-toepfer@users.noreply.github.com>
2023-05-13 15:03:36 +0200

    add more protected branch configs

--- github_branch_protection.tf
+++ github_branch_protection.tf
@@ -24,3 +24,55 @@ resource "github_branch_protection" "sebastian-toepfer_domain-driven-desgin" {
   }
 }
 
+resource "github_branch_protection" "sebastian-toepfer_json-rpc" {
+  repository_id = github_repository.sebastian-toepfer_json-rpc.node_id
+
+  pattern          = "main"
+  enforce_admins   = true
+  allows_deletions = false
+
+  require_conversation_resolution = true
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    dismissal_restrictions          = []
+    pull_request_bypassers          = []
+    require_code_owner_reviews      = false
+    require_last_push_approval      = true
+    required_approving_review_count = 0
+    restrict_dismissals             = false
+  }
+
+  required_status_checks {
+    contexts = [
+      "build / build and analyze",
+    ]
+    strict = true
+  }
+}
+
+resource "github_branch_protection" "sebastian-toepfer_pdfbox-fluent-layout" {
+  repository_id = github_repository.sebastian-toepfer_pdfbox-fluent-layout.node_id
+
+  pattern          = "main"
+  enforce_admins   = true
+  allows_deletions = false
+
+  require_conversation_resolution = true
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    dismissal_restrictions          = []
+    pull_request_bypassers          = []
+    require_code_owner_reviews      = false
+    require_last_push_approval      = true
+    required_approving_review_count = 0
+    restrict_dismissals             = false
+  }
+
+  required_status_checks {
+    contexts = [
+      "build / build and analyze",
+    ]
+    strict = true
+  }
+}
+
