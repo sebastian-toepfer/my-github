6262aa3ab07ca0c6512ed2fa5b008b2b0f825237
Sebastian Toepfer <61313468+sebastian-toepfer@users.noreply.github.com>
2023-05-13 17:02:13 +0200

    Revert "Merge pull request #3 from sebastian-toepfer/gh_action_again"
    This reverts commit 915fc8ab9df33951eedb0b2dd273827fefc7c0d2, reversing
    changes made to f9ff5f833057f1ee1b51d0b23f22df3bb046537d.

--- github_actions_secret.tf
+++
@@ -1,6 +0,0 @@
-resource "github_actions_secret" "sebastian-toepfer_my-github-terraform-secret" {
-  repository      = github_repository.sebastian-toepfer_my-github.name
-  secret_name     = "TF_API_TOKEN"
-  plaintext_value = var.gh_actions-secret-terraform_default
-}
-
--- variable.tf
+++ variable.tf
@@ -2,8 +2,3 @@ variable "github_token" {
   type = string
 }
 
-variable "gh_actions-secret-terraform_default" {
-  type    = string
-  default = "secret"
-}
-
