7e6325bce18493815dbfb7a8b84fba113a1a8022
Sebastian Toepfer <61313468+sebastian-toepfer@users.noreply.github.com>
2023-06-08 11:22:37 +0200

    add secrets for publish to all repos

--- sebastian-toepfer/04_github_actions_secret.tf
+++ sebastian-toepfer/04_github_actions_secret.tf
@@ -10,6 +10,30 @@ resource "github_actions_secret" "domain-driven-desgin_unprotect-the-protected" 
   plaintext_value = var.unprotect-the-protected
 }
 
+resource "github_actions_secret" "domain-driven-desgin_mvn-user" {
+  repository      = github_repository.domain-driven-desgin.name
+  secret_name     = "mvn_username"
+  plaintext_value = var.mvn_repo_user
+}
+
+resource "github_actions_secret" "domain-driven-desgin_mvn-pwd" {
+  repository      = github_repository.domain-driven-desgin.name
+  secret_name     = "mvn_password"
+  plaintext_value = var.mvn_repo_pwd
+}
+
+resource "github_actions_secret" "domain-driven-desgin_gpg-pwd" {
+  repository      = github_repository.domain-driven-desgin.name
+  secret_name     = "GPG_PASSPHRASE"
+  plaintext_value = var.gpg_pwd
+}
+
+resource "github_actions_secret" "domain-driven-desgin_gpg-key" {
+  repository      = github_repository.domain-driven-desgin.name
+  secret_name     = "GPG_PRIVATE_KEY"
+  plaintext_value = var.gpg_key
+}
+
 resource "github_actions_secret" "pdfbox-fluent-layout_sonar-token" {
   repository      = github_repository.pdfbox-fluent-layout.name
   secret_name     = "sonar_token"
@@ -22,6 +46,30 @@ resource "github_actions_secret" "pdfbox-fluent-layout_unprotect-the-protected" 
   plaintext_value = var.sonar-token
 }
 
+resource "github_actions_secret" "pdfbox-fluent-layout_mvn-user" {
+  repository      = github_repository.pdfbox-fluent-layout.name
+  secret_name     = "mvn_username"
+  plaintext_value = var.mvn_repo_user
+}
+
+resource "github_actions_secret" "pdfbox-fluent-layout_mvn-pwd" {
+  repository      = github_repository.pdfbox-fluent-layout.name
+  secret_name     = "mvn_password"
+  plaintext_value = var.mvn_repo_pwd
+}
+
+resource "github_actions_secret" "pdfbox-fluent-layout_gpg-pwd" {
+  repository      = github_repository.pdfbox-fluent-layout.name
+  secret_name     = "GPG_PASSPHRASE"
+  plaintext_value = var.gpg_pwd
+}
+
+resource "github_actions_secret" "pdfbox-fluent-layout_gpg-key" {
+  repository      = github_repository.pdfbox-fluent-layout.name
+  secret_name     = "GPG_PRIVATE_KEY"
+  plaintext_value = var.gpg_key
+}
+
 resource "github_actions_secret" "json-rpc_sonar-token" {
   repository      = github_repository.json-rpc.name
   secret_name     = "sonar_token"
@@ -32,6 +80,30 @@ resource "github_actions_secret" "json-rpc_unprotect_the_protected" {
   repository      = github_repository.json-rpc.name
   secret_name     = "unprotect_the_protected"
   plaintext_value = var.unprotect-the-protected
+}
+
+resource "github_actions_secret" "json-rpc_mvn-user" {
+  repository      = github_repository.json-rpc.name
+  secret_name     = "mvn_username"
+  plaintext_value = var.mvn_repo_user
+}
+
+resource "github_actions_secret" "json-rpc_mvn-pwd" {
+  repository      = github_repository.json-rpc.name
+  secret_name     = "mvn_password"
+  plaintext_value = var.mvn_repo_pwd
+}
+
+resource "github_actions_secret" "json-rpc_gpg-pwd" {
+  repository      = github_repository.json-rpc.name
+  secret_name     = "GPG_PASSPHRASE"
+  plaintext_value = var.gpg_pwd
+}
+
+resource "github_actions_secret" "json-rpc_gpg-key" {
+  repository      = github_repository.json-rpc.name
+  secret_name     = "GPG_PRIVATE_KEY"
+  plaintext_value = var.gpg_key
 }
 
 resource "github_actions_secret" "json-printable-maven-plugin_sonar-token" {
@@ -69,4 +141,3 @@ resource "github_actions_secret" "json-printable-maven-plugin_gpg_key" {
   secret_name     = "GPG_PRIVATE_KEY"
   plaintext_value = var.gpg_key
 }
-
